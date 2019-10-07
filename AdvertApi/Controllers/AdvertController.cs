5cb3d8caf9faf7199e321b2d84a8f536df33ec2b
Jeverson Morais <jeverson@gmail.com>
2019-10-07 15:09:02 -0300

    Create and Update ads in DynamoDb

---
+++ AdvertApi/Controllers/AdvertController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using AdvertApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AdvertApi.Controllers
+{
+    [ApiController]
+    [Route("adverts/v1")]
+    public class AdvertController : ControllerBase
+    {
+        private readonly IAdvertStorageService _advertStorageService;
+
+        public AdvertController(IAdvertStorageService advertStorageService)
+        {
+            _advertStorageService = advertStorageService;
+        }
+
+        [HttpPost]
+        [Route("Create")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(200, Type = typeof(CreateAdvertResponse))]
+        public async Task<IActionResult> Create(AdvertModel model)
+        {
+            try
+            {
+                var id = await _advertStorageService.Add(model);
+                return StatusCode(201, new CreateAdvertResponse { Id = id });
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+
+        [HttpPut]
+        [Route("Confirm")]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<IActionResult> Confirm(ConfirmAdvertModel model)
+        {
+            try
+            {
+                await _advertStorageService.Confirm(model);
+                return new OkResult();
+            }
+            catch (KeyNotFoundException)
+            {
+                return new NotFoundResult();
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+        }
+    }
+}
---
+++ AdvertApi/Services/DynamoDbAdvertStorage.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AdvertApi.Models;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using AutoMapper;
+
+namespace AdvertApi.Services
+{
+    public class DynamoDbAdvertStorage : IAdvertStorageService
+    {
+        private readonly IMapper _mapper;
+
+        public DynamoDbAdvertStorage(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        public async Task<string> Add(AdvertModel model)
+        {
+            var dbModel = _mapper.Map<AdvertDbModel>(model);
+            dbModel.Id = new Guid().ToString();
+            dbModel.CreationDateTime = DateTime.UtcNow;
+            dbModel.Status = AdvertStatus.Pending;
+
+            using (var client = new AmazonDynamoDBClient())
+                using (var ctx = new DynamoDBContext(client))
+                    await ctx.SaveAsync(dbModel);
+
+            return dbModel.Id;
+        }
+
+        public async Task Confirm(ConfirmAdvertModel model)
+        {
+            using (var client = new AmazonDynamoDBClient())
+            {
+                using (var ctx = new DynamoDBContext(client))
+                {
+                    var record = await ctx.LoadAsync<AdvertDbModel>(model.Id);
+                    if (record == null)
+                        throw new KeyNotFoundException($"A record with Id={model.Id} was not found.");
+
+                    if (model.Status == AdvertStatus.Active)
+                        await UpdateAdvertAsActive(ctx, record);
+                    else
+                        await DeleteAdvert(ctx, record);
+                }
+            }
+        }
+
+        private static async Task DeleteAdvert(DynamoDBContext ctx, AdvertDbModel record)
+        {
+            await ctx.DeleteAsync(record);
+        }
+
+        private static async Task UpdateAdvertAsActive(DynamoDBContext ctx, AdvertDbModel record)
+        {
+            record.Status = AdvertStatus.Active;
+            await ctx.SaveAsync(record);
+        }
+    }
+}
